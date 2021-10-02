10ebdb6bfae6f905ad414d9bc85462d11c0988ce
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-10-03 00:07:47 +0900

    싱글톤과 데이터용 클래스 추가

---
+++ TrickyTowerDefense/Assets/Scripts/Base/MonoSingleton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<GameManager> : MonoBehaviour where GameManager : MonoBehaviour
+{
+    private static bool isShutDown = false;
+
+    private static object locker = new object();
+
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+
+            if(isShutDown)
+            {
+                return null;
+            }
+
+            lock(locker)
+            {
+                if (!instance)
+                {
+                    instance = (GameManager)FindObjectOfType(typeof(GameManager));
+                    if (!instance)
+                    {
+                        GameObject temp = new GameObject(typeof(GameManager).ToString());
+                        instance = temp.AddComponent<GameManager>();
+                    }
+
+                    DontDestroyOnLoad(instance);
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        isShutDown = true;
+    }
+    private void OnDestroy()
+    {
+        isShutDown = true;
+    }
+    private void OnEnable()
+    {
+        isShutDown = false;
+    }
+
+}
