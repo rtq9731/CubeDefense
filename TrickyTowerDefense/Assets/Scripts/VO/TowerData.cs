c85debc7f0811a2439e36112029b5343b3400146
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-10-03 19:15:45 +0900

    타워 데이터 클래스 생성

--- TrickyTowerDefense/Assets/Scripts/VO/TowerData.cs
+++ TrickyTowerDefense/Assets/Scripts/VO/TowerData.cs
@@ -1,8 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class TowerData
 {
+    public TowerType myType = 0;
+    public TowerGrade myGrade = 0;
 
+    public Vector2 position = Vector2.zero;
+
+    public int atk;
+    public int atkSpeed;
+}
+
+public enum TowerType
+{
+    Grenadier,
+    Laser,
+    Acher,
+    Bullet,
+    Buff
+}
+
+public enum TowerGrade
+{
+    Common,
+    Uncommon,
+    Rare,
+    Epic,
+    Unique,
+    Legendary
 }
