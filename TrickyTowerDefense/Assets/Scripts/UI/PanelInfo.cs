1374b3090795774a5a1db03820548b10a2274985
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-10-05 22:04:25 +0900

    사전 페이지 레이아웃 구성 및 그에 따른 스크립트 작성

--- TrickyTowerDefense/Assets/Scripts/UI/PanelInfo.cs
+++ TrickyTowerDefense/Assets/Scripts/UI/PanelInfo.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Text textTowerName;
 
-    // Update is called once per frame
-    void Update()
+    public void InitInfoPanel(TowerData data)
     {
-        
+
     }
 }
--- TrickyTowerDefense/Assets/Scripts/UI/TowerWikiIconElement.cs
+++ TrickyTowerDefense/Assets/Scripts/UI/TowerWikiIconElement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerWikiElement : MonoBehaviour
+public class TowerWikiIconElement : MonoBehaviour
 {
     [SerializeField] TowerData data;
     
