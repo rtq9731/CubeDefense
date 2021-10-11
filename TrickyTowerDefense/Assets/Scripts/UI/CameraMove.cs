462abb1b95ee7a5ed9c4ab99f589ed9b4276ec05
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-10-11 21:46:22 +0900

    카메라 움직이지 않는 버그 해결

--- TrickyTowerDefense/Assets/Scripts/UI/CameraMove.cs
+++ TrickyTowerDefense/Assets/Scripts/UI/CameraMove.cs
@@ -12,18 +12,16 @@ public class CameraMove : MonoBehaviour
 
     float verticalAxis;
 
-    private void Start()
-    {
-        verticalAxis = Input.GetAxis("Vertical");
-    }
-
     private void Update()
     {
+        verticalAxis = Input.GetAxis("Vertical");
         if (verticalAxis != 0)
         {
             movePos = transform.position;
             movePos.y += speed * Time.deltaTime * verticalAxis;
-            movePos.y = Mathf.Clamp(movePos.y, 7, yPositionLimit); // 임시로 30으로 두겟음. 타워 높이따라 달라지게 하면 될듯.
+            Debug.Log(movePos.y);
+            movePos.y = Mathf.Clamp(movePos.y, 7, yPositionLimit);
+            Debug.Log(movePos.y + " " + yPositionLimit);
             transform.position = movePos;
         }
     }
