ebc1e3bc409297ac57b38540cf507d449c826d53
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-11-27 19:25:02 +0900

    EnemyScript 추가

--- TrickyTowerDefense/Assets/Scripts/Unit/EnemyScript.cs
+++ TrickyTowerDefense/Assets/Scripts/Unit/EnemyScript.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    EnemyData data = new EnemyData();
+    EnemyHealth health = null;
 
+    public EnemyData Data
+    {
+        get { return data; }
+    }
+
+    private void Start()
+    {
+        health = GetComponent<EnemyHealth>();
+    }
 }
