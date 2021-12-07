45f2df680a9a115a5628ffc649d1009cd59d3be5
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-12-07 23:17:46 +0900

    대기 골드 구현

--- TrickyTowerDefense/Assets/Scripts/System/StageManager.cs
+++ TrickyTowerDefense/Assets/Scripts/System/StageManager.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class StageManager : MonoBehaviour
 {
     [SerializeField] int stage = 0;
-    [SerializeField] float stageTime = 60f;
+    [SerializeField] float stageTime = 100f;
     [SerializeField] float stageWaitTime = 20f;
+    public int plusGold = 0; // 라운드 끝날때까지 기다린다음 골드 지급
 
     StageTimer stageTimer = null;
 
@@ -14,5 +15,6 @@ public class StageManager : MonoBehaviour
     {
         stageTimer = GetComponent<StageTimer>();
         stageTimer.SetStageTimer(stageTime, stageWaitTime);
+        stageTimer._onStageStart += () => { GameManager.Instance.GetData().Gold += plusGold; };
     }
 }
