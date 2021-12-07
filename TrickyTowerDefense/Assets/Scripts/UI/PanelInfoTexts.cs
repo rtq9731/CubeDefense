6fb99f148f964c1ce3d41952c5a254cfc8a38522
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-12-07 23:19:51 +0900

    여러가지 변동사항

--- TrickyTowerDefense/Assets/Scripts/UI/PanelInfoTexts.cs
+++ TrickyTowerDefense/Assets/Scripts/UI/PanelInfoTexts.cs
@@ -17,18 +17,18 @@ public class PanelInfoTexts : MonoBehaviour
 
     public void InitTexts(PlayerData data)
     {
-        textCurRound.text = $"현재 라운드 : {data.Round++}";
+        textCurRound.text = $"현재 라운드 : {data.Round + 1}";
         textCurHP.text = $"남은 체력 : {data.HP}";
-        textCurGold.text = $"현재 골드 : {data.Gold}";
+        textCurGold.text = $"현재 골드 : {data.Gold} + {GameManager.Instance.stageManager.plusGold} (대기)";
         textIsOnRound.text = $"스테이지 준비 시간입니다.";
     }
 
     public void UpdateTexts()
     {
         PlayerData data = GameManager.Instance.GetData();
-        textCurRound.text = $"현재 라운드 : {data.Round}";
+        textCurRound.text = $"현재 라운드 : {data.Round + 1}";
         textCurHP.text = $"남은 체력 : {data.HP}";
-        textCurGold.text = $"현재 골드 : {data.Gold}";
+        textCurGold.text = $"현재 골드 : {data.Gold} + {GameManager.Instance.stageManager.plusGold} (대기)";
     }
 
     public void UpdateStageTextOnStartStage()
