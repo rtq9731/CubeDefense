586ac2511328c0c7a575df7ca9ee6a7bad5bcbae
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-12-12 00:07:42 +0900

    타워 업그레이드를 저장할 수 있도록 데이터화

--- TrickyTowerDefense/Assets/Scripts/VO/PlayerData.cs
+++ TrickyTowerDefense/Assets/Scripts/VO/PlayerData.cs
@@ -9,8 +9,9 @@ public class PlayerData : ISerializationCallbackReceiver
     [SerializeField] private List<TowerData> towerDatas = new List<TowerData>();
     [SerializeField] private int gold = 10;
     [SerializeField] private int round = 0;
-    [SerializeField] private int hp = 0;
-    [SerializeField]private Dictionary<TowerData.TowerType, int> upgradeCountDict = new Dictionary<TowerData.TowerType, int>() 
+    [SerializeField] private int hp = 100;
+
+    [SerializeField] private Dictionary<TowerData.TowerType, int> upgradeCountDict = new Dictionary<TowerData.TowerType, int>()
     { 
         { TowerData.TowerType.Grenadier, 0 }, 
         { TowerData.TowerType.Laser, 0 }, 
@@ -19,6 +20,14 @@ public class PlayerData : ISerializationCallbackReceiver
         { TowerData.TowerType.Buff, 0 } 
     };
 
+    [SerializeField] private Dictionary<TowerData.TowerType, int> upgradePriceDict = new Dictionary<TowerData.TowerType, int>()
+    {
+        { TowerData.TowerType.Grenadier, 1 },
+        { TowerData.TowerType.Laser, 1 },
+        { TowerData.TowerType.Acher, 1 },
+        { TowerData.TowerType.Bullet, 1 },
+        { TowerData.TowerType.Buff, 1 }
+    };
 
     public int Gold
     {
@@ -35,7 +44,10 @@ public class PlayerData : ISerializationCallbackReceiver
     public int HP
     {
         get { return hp; }
-        set { hp = value; }
+        set { 
+            hp = value;
+
+        }
     }
 
     public bool Buy(int price)
@@ -44,6 +56,7 @@ public class PlayerData : ISerializationCallbackReceiver
             return false;
 
         gold -= price;
+        GameManager.Instance.uiManager.infoTexts.UpdateTexts();
         return true;
     }
 
@@ -52,20 +65,33 @@ public class PlayerData : ISerializationCallbackReceiver
         get { return upgradeCountDict; }
     }
 
+    public Dictionary<TowerData.TowerType, int> UpgradePriceDict
+    {
+        get { return upgradePriceDict; }
+    }
+
     public List<TowerData> TowerDatas => towerDatas;
 
     public void OnBeforeSerialize()
     {
-        towerDatas = GameManager.Instance.towerManager.GetAllLivingTowerData();
-        for (int i = 0; i < towerDatas.Count; i++)
+        if (Application.isPlaying)
         {
-            towerDatas[i].Position = towers[i].transform.position;
+            if (GameManager.Instance != null)
+            {
+                towers = GameManager.Instance.towerManager.GetAllLivingTowerData();
+                foreach (var item in towers)
+                {
+                    item.TowerData.Position = item.transform.position;
+                }
+            }
         }
-
     }
 
     public void OnAfterDeserialize()
     {
-
+        foreach (var item in towers)
+        {
+            GameObject.FindObjectOfType<TowerManager>().GetNewTower(item.TowerData.Idx).transform.position = item.TowerData.Position;
+        }
     }
 }
