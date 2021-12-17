ef519f81f96c0f9909f498874c921f60d349ee9d
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-12-17 09:36:28 +0900

    이펙트 만들기 위해 이미지 추가

--- TrickyTowerDefense/Assets/Scripts/EffectManager.cs
+++ TrickyTowerDefense/Assets/Scripts/EffectManager.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum effectType
+{
+    ExplosionEffect,
+    laserEffect,
+    PowerUpEffect
+}
 public class EffectManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
